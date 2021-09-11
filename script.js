316ce52343edc703d3800d861f3557629faf0275
Marat123480 <78693401+Marat123480@users.noreply.github.com>
2021-09-11 17:59:48 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ function Back(id) {
     }, 1000);
 
 
-}   
-function ChangeSlide(id){
-    if(id == "pager-1"){
+}
+function ChangeSlide(id) {
+    if (id == "pager-1") {
         document.getElementById("one").style.display = "flex";
         document.getElementById("pager-1").style.background = "black";
         document.getElementById("two").style.display = "none";
@@ -24,7 +24,7 @@ function ChangeSlide(id){
         document.getElementById("three").style.display = "none";
         document.getElementById("pager-3").style.background = "#2bcc91";
     }
-    if(id == "pager-2"){
+    if (id == "pager-2") {
         document.getElementById("one").style.display = "none";
         document.getElementById("pager-1").style.background = "#2bcc91";
         document.getElementById("two").style.display = "flex";
@@ -32,7 +32,7 @@ function ChangeSlide(id){
         document.getElementById("three").style.display = "none";
         document.getElementById("pager-3").style.background = "#2bcc91";
     }
-    if(id == "pager-3"){
+    if (id == "pager-3") {
         document.getElementById("one").style.display = "none";
         document.getElementById("pager-1").style.background = "#2bcc91";
         document.getElementById("two").style.display = "none";
@@ -40,4 +40,25 @@ function ChangeSlide(id){
         document.getElementById("three").style.display = "flex";
         document.getElementById("pager-3").style.background = "black";
     }
+}
+
+function loadPage() {
+    body = document.getElementById("body");
+    head = document.getElementById("head");
+    section = document.getElementById("main");
+    footer = document.getElementById("foot");
+    head.style.display = "none";
+    section.style.display = "none";
+    footer.style.display = "none";
+    body.style.background = "url(images/loading.gif)";
+    body.style.backgroundRepeat = "no-repeat";
+    body.style.backgroundSize = "50%";
+    body.style.backgroundPosition = "top";
+    body.style.backgroundColor = "#FCFEFC";
+    setTimeout(() => {
+        head.style.display = "block";
+        section.style.display = "block";
+        footer.style.display = "flex";
+        body.style.background = "white";
+    }, 2000);
 }
